fab514efc4379474e156ce9c5b71dad38b3b95ac
[Victor Akinyemi] <[victorakinyemi52@gmail.com]>
2024-07-31 21:37:46 +0100

    updated files

---
+++ ORANGOTAN.js
@@ -0,0 +1,51 @@
+document.getElementById('home');
+document.getElementById('movies');
+document.getElementById('animes');
+document.getElementById('cartoons');
+document.getElementById('serie');
+document.getElementById('searchInput');
+//document.addEventListener('searchInput');
+document.getElementById('profileHolder');
+document.getElementById('premiumOption');
+//.addEventListener('premiumoption');
+document.getElementById('modeSwitchBall');
+//document.addEventListener('modeSwitchBall');
+document.getElementById('closeIcon');
+//document.addEventListener('closeIcon');
+document.getElementById('forCursor');
+//document.addEventListener('forCursor');
+document.getElementById('donateButton');
+//document.addEventListener('donateButtons');
+document.getElementById('contactButton');
+//.addEventListener('contactButton');
+document.getElementById('submitRequest');
+//document.addEventListener('submitRequest');
+
+function navHome(){
+    let home = documet.getElementById('home');
+    home
+}
+
+function navMovies(){
+
+}
+
+function navAnime()[
+
+]
+
+function navCartoons(){
+
+}
+
+function navSeries(){
+
+}
+
+function modeSwitcher(){
+    let body = document.getElementById('body');
+    body.style.backgroundColor = "#000000";
+
+}
+
+
